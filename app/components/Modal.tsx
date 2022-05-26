91db170daffe2f3cfa90a038246779ae6f550201
Jesse Strangfeld <jesse.strangfeld@gmail.com>
2022-05-26 16:03:20 -0400

    update comment form design and remove county

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -1,4 +1,3 @@
-import React, { useState } from "react";
 import CommentForm from "./CommentForm";
 
 export interface VisibilityProps {
@@ -9,13 +8,10 @@ export interface VisibilityProps {
 }
 
 export default function Modal({ isVisible, setIsVisible }: VisibilityProps) {
+  const display = isVisible.visibility ? "fixed" : "hidden";
   return (
     <div
-      className="bg-stone-700 fixed modal mt-4 p-2 rounded text-white w-1/3"
-      style={{
-        display: isVisible.visibility ? "" : "none",
-        zIndex: 1000,
-      }}
+      className={`${display} bg-stone-700 modal mt-20 p-2 rounded text-white w-1/3 z-50`}
     >
       <CommentForm isVisible={isVisible} setIsVisible={setIsVisible} />
     </div>
