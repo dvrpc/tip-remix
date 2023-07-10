9724fb0742dbfd6b3179dc0344a99f7c7c6b28f8
Tyler Hachadorian <49740237+hachadorian@users.noreply.github.com>
2023-07-10 11:52:02 -0400

    updated funds endpoint

--- app/fund.ts
+++ app/fund.ts
@@ -1,4 +1,4 @@
 export async function getFunds() {
-  const data = await fetch("https://www.dvrpc.org/data/tip/2023/funds");
+  const data = await fetch("https://www.dvrpc.org/data/tip/2024/funds");
   return await data.json();
 }
