b50d73f853ce8aa3092770b21ed9a08b5b27606c
hachadorian <hachadorian98@gmail.com>
2024-05-22 12:59:08 -0400

    updated endpoints

--- app/fund.ts
+++ app/fund.ts
@@ -1,4 +1,4 @@
 export async function getFunds() {
-  const data = await fetch("https://www.dvrpc.org/data/tip/2023/funds");
+  const data = await fetch("https://www.dvrpc.org/data/tip/2025/funds");
   return await data.json();
 }
--- app/project.ts
+++ app/project.ts
@@ -1,16 +1,16 @@
 export async function getProjects() {
-  const data = await fetch("https://www.dvrpc.org/data/TIP/2023/list");
+  const data = await fetch("https://www.dvrpc.org/data/TIP/2025/list");
   return await data.json();
 }
 
 export async function getProject(id: string) {
-  const data = await fetch(`https://www.dvrpc.org/data/TIP/2023/id/${id}`);
+  const data = await fetch(`https://www.dvrpc.org/data/TIP/2025/id/${id}`);
   return await data.json();
 }
 
 export async function searchProjects(keyword: string | null, filters = "") {
   const data = await fetch(
-    `https://www.dvrpc.org/data/TIP/2023/list/${
+    `https://www.dvrpc.org/data/TIP/2025/list/${
       keyword ? keyword : "_"
     }/${filters}`
   );
