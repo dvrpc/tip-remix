21f8bf82d3f63f2dc962ce4bc0278e534badd36d
hachadorian <hachadorian98@gmail.com>
2024-10-31 12:29:27 -0400

    updated boundary styling

--- app/mapbox-layers.ts
+++ app/mapbox-layers.ts
@@ -13,7 +13,7 @@ export const boundaryLayers = [
       type: "line",
       paint: {
         "line-color": "#6e6e6e",
-        "line-width": 1.75,
+        "line-width": 2,
       },
     },
   },
@@ -27,7 +27,7 @@ export const boundaryLayers = [
       type: "line",
       paint: {
         "line-color": "#6e6e6e",
-        "line-width": 1.75,
+        "line-width": 2,
       },
     },
   },
@@ -41,7 +41,7 @@ export const boundaryLayers = [
       type: "line",
       paint: {
         "line-color": "#6e6e6e",
-        "line-width": 1.75,
+        "line-width": 2,
       },
     },
   },
