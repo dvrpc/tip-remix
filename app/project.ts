fb938837355ec28a4f6cde1afb42a836907ed0f9
hachadorian <hachadorian98@gmail.com>
2025-07-18 11:37:53 -0400

    projects and funds 26 endpoints

--- app/project.ts
+++ app/project.ts
@@ -1,16 +1,16 @@
 export async function getProjects() {
-  const data = await fetch("https://www.dvrpc.org/data/TIP/2024/list");
+  const data = await fetch("https://www.dvrpc.org/data/TIP/2026/list");
   return await data.json();
 }
 
 export async function getProject(id: string) {
-  const data = await fetch(`https://www.dvrpc.org/data/TIP/2024/id/${id}`);
+  const data = await fetch(`https://www.dvrpc.org/data/TIP/2026/id/${id}`);
   return await data.json();
 }
 
 export async function searchProjects(keyword: string | null, filters = "") {
   const data = await fetch(
-    `https://www.dvrpc.org/data/TIP/2024/list/${
+    `https://www.dvrpc.org/data/TIP/2026/list/${
       keyword ? keyword : "_"
     }/${filters}`
   );
